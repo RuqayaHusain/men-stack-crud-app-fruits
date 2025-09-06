f12691394a451836aeb75b1986c3af1717a26ee0
Ruqaya Husain <ruqayanader22@gmail.com>
2025-09-06 12:49:07 +0300

    finish till build the fruits show page

--- server.js
+++ server.js
@@ -23,6 +23,13 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.get('/fruits', async (req, res) => {
+    const fruits = await Fruit.find({}); // or .find() which will print all of the fruits
+    res.render('fruits/index.ejs', {
+        fruits: fruits,
+    });
+});
+
 app.post('/fruits', async (req, res) => {
     if(req.body.isReadyToEat === 'on') {
         req.body.isReadyToEat = true;
@@ -31,11 +38,17 @@ app.post('/fruits', async (req, res) => {
     }
     await Fruit.create(req.body); // passes the javascript object to be created using the exported Fruit model
     // console.log(req.body); // print the encoded object as javascript object
-    res.redirect('/fruits/new'); //redirect triggers a get request (we add routes as an argument)
+    res.redirect('/fruits'); //redirect triggers a get request (we add routes as an argument) to the index page if it exists
 })
 
 app.get('/fruits/new', (req, res) => {
     res.render('fruits/new.ejs');
+});
+
+app.get("/fruits/:fruitId", async (req, res) => {
+    const fruitId = req.params.fruitId;
+    const fruit = await Fruit.findById(fruitId);
+    res.render('fruits/show.ejs', { fruit });
 });
 
 app.listen(PORT, () => {
